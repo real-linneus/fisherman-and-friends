63f813bb58eeeb3ea245bbb1e3c99981df31ef03
Oskar Kolte <oskar@Oskars-MacBook-Pro-2.local>
2021-11-30 20:46:08 +0100

    figurer

--- InitializeFigure.m
+++ InitializeFigure.m
@@ -1,5 +1,8 @@
 figure(1); hold on;
+axis square
+axis(environment.lakeSize*[-1/2, 1/2,-1/2, 1/2])
 for i = 1:length(agents)
-    environment.scatter = scatter(0,0,1);
-    environment.quiver = quiver(0,0,0,0);
+    environment.scatter(i) = scatter(agents(i).x,agents(i).y,agents(i).radius,agents(i).plotColor,'filled','DisplayName',agents(i).name);
 end
+environment.quiver = quiver([agents.x],[agents.y],[agents.velocity].*cos([agents.angle]),[agents.velocity].*sin([agents.angle]),'r','LineWidth',1);
+legend(environment.scatter)
