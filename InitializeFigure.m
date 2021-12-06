2158ac169d3f408155984deebc4f0566851221bb
Oskar Kolte <oskar@Oskars-MacBook-Pro-2.local>
2021-12-06 16:33:05 +0100

    radiuses now work!

--- InitializeFigure.m
+++ InitializeFigure.m
@@ -1,12 +1,15 @@
 figure(1); hold on;
-set(gca,'Units','points')
+set(gca,'Units','points','Color',[0.7,0.7,0.7])
 axis square
 axis(environment.lakeSize*[-1/2, 1/2,-1/2, 1/2])
 scaleFactor = get(gca,'Position');
 scaleFactor = scaleFactor(4)/environment.lakeSize;
 for i = 1:length(agents)
     s = (2*scaleFactor*agents(i).radius)^2;
-    environment.scatter(i) = scatter(agents(i).x,agents(i).y,s,agents(i).plotColor,'filled','DisplayName',agents(i).name);
+    environment.scatter(i) = scatter(agents(i).x,agents(i).y,s,agents(i).plotColor,'filled','DisplayName',agents(i).name,'MarkerFaceAlpha',0.7);
+end
+for i = environment.scatter(contains({environment.scatter.DisplayName},"Fisherman"))
+    uistack(i,'top');
 end
 environment.quiver = quiver([agents.x],[agents.y],[agents.velocity].*cos([agents.angle]),[agents.velocity].*sin([agents.angle]),'r','LineWidth',1,'AutoScale','off');
-legend(environment.scatter)
+legend(environment.scatter,'Location','EastOutside')
