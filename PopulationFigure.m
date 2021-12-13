d72ed6d7a73c173485c41bbf437b23fc80532533
Hannes Johansson <hannes.johansson@astecon.se>
2021-12-13 11:21:02 +0100

    Fixing labels.

--- PopulationFigure.m
+++ PopulationFigure.m
@@ -4,5 +4,5 @@ hold on
 plot(populations(1,:),'-.r')
 plot(populations(2,:),'-b')
 plot(populations(3,:),'--g')
-legend('Fisherman','Big Fish','Small Fish')
+legend('Fisherman (load of fish)','Big Fish','Small Fish')
 title('Populations')
