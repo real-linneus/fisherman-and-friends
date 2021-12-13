87770f71b8ad6e666667d5a96aae1b6de5867d8b
Hannes Johansson <hannes.johansson@astecon.se>
2021-12-13 09:58:51 +0100

    Fixing graphs.

--- AverageCatchFigure.m
+++ AverageCatchFigure.m
@@ -1,7 +1,14 @@
-figure(5)
+figure(6)
 clf
 hold on
-plot(averageCatch(1,:),'-.r')
+plot(averageCatch(1,:),'.r')
+plot(averageCatch(3,:),'-b','Linewidth',2)
+legend('Average catch per dt','Average total catch / #dt')
+title('Average catch')
+
+figure(7)
+clf
+hold on
 plot(averageCatch(2,:),'-b')
-plot(averageCatch(3,:),'--g')
-legend('Catch per dt','Total catch','Total catch per #dt')
+legend('Average catch per dt')
+title('Average catch per dt')
